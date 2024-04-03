d12835880d92d72b7c4bd947bc83cc8a12240dab
Kev-rod43 <kev.rod43@yahoo.com>
2024-04-03 17:40:39 -0400

    wrote all client-side graphql queries with the exception of SAVE_BOOK

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,35 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String, $username: String, $password: String!){
+        login(email: $email, username: $username, password: $password){
+            token: ID!
+            user: User
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!){
+        addUser(username: $username, email: $email, password: $password){
+            token: ID!
+            user: User
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook()
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!){
+        removeBook(bookId: $bookId){
+            _id: ID!
+            username: String!
+            email: String!
+            bookCount: Int!
+            savedBooks: [Book]
+        }
+    }
+`;
