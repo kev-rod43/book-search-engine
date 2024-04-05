913a6a3ad90f34b1bc9e1772ebddc662a429c0eb
Kev-rod43 <kev.rod43@yahoo.com>
2024-04-05 00:07:34 -0400

    fixed all bugs, finished the pages

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,8 +3,21 @@ import { gql } from '@apollo/client';
 export const LOGIN_USER = gql`
     mutation loginUser($email: String, $username: String, $password: String!){
         login(email: $email, username: $username, password: $password){
-            token: ID!
-            user: User
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    authors
+                    description
+                    bookId
+                    image
+                    link
+                    title
+                }
+            }
         }
     }
 `;
@@ -12,8 +25,21 @@ export const LOGIN_USER = gql`
 export const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!){
         addUser(username: $username, email: $email, password: $password){
-            token: ID!
-            user: User
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    authors
+                    description
+                    bookId
+                    image
+                    link
+                    title
+                }
+            }
         }
     }
 `;
@@ -21,11 +47,18 @@ export const ADD_USER = gql`
 export const SAVE_BOOK = gql`
     mutation saveBook($input: BookInput){
         saveBook(input: $input){
-            _id: ID!
-            username: String!
-            email: String!
-            bookCount: Int!
-            savedBooks: [Book]
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
         }
     }
 `;
@@ -33,11 +66,18 @@ export const SAVE_BOOK = gql`
 export const REMOVE_BOOK = gql`
     mutation removeBook($bookId: String!){
         removeBook(bookId: $bookId){
-            _id: ID!
-            username: String!
-            email: String!
-            bookCount: Int!
-            savedBooks: [Book]
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
         }
     }
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,12 +2,18 @@ import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
   query me {
-    User {
-        _id
-        username
-        email
-        bookCount
-        savedBooks
+    me {
+      _id
+      bookCount
+      email
+      savedBooks {
+        link
+        title
+        image
+        description
+        bookId
+        authors
+      }
     }
   }
 `;
