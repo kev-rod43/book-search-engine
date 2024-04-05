dc32176dbf20525cf94c3a00397325b576136f38
Kev-rod43 <kev.rod43@yahoo.com>
2024-04-04 22:59:02 -0400

    added missing middleware util

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
-const jwt = require('jsonwebtoken');
 const { GraphQLError } = require('graphql');
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretssshhhhhhh';
 const expiration = '2h';
 
 module.exports = {
@@ -10,10 +10,29 @@ module.exports = {
       code: 'UNAUTHENTICATED',
     },
   }),
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
 
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
+
